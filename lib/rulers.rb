2625d78e5f83bd28275a49677e70ab1096764bff
ethanchen <ethan@fresho.com>
2023-10-14 18:25:38 +1100

    setup minitest

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
-
+require "rulers/array"
 require_relative "rulers/version"
 
 module Rulers
   class Error < StandardError; end
   # Your code goes here...
+  #
+  class Application
+    def call(env)
+      [
+        200,
+        {'Content-Type'=>'text/html'},
+        ["Hello from Ruby on Rulers!"]
+      ]
+    end
+  end
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "rulers"
+require "rack/test"
+
+require "minitest/autorun"
